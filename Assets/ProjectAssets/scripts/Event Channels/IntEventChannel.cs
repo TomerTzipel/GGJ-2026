2ddb840ab5769a213409631261706b3a81d3083e
TomerTzipel <pporpyu@gmail.com>
2026-01-30 02:45:22 +0200

    Base Movement

---
+++ Assets/ProjectAssets/scripts/Event Channels/IntEventChannel.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IntEventChannel", menuName = "Scriptable Objects/Event Channels/IntEventChannel")]
+public class IntEventChannel : EventChannel<int>
+{}
