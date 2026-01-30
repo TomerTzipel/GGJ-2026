86df269ff3a6e32ca523a8782a61177d633f5426
TomerTzipel <pporpyu@gmail.com>
2026-01-30 15:36:38 +0200

    prefab is back

---
+++ Assets/ProjectAssets/scripts/Projectile/ProjectileHandler.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+
+
+public class ProjectileHandler : MonoBehaviour
+{
+
+    public Vector2 MoveDirection { get; set; }
+    public bool IsPlayerProjectile { get; set; }
+    public MaskType CurrentMask { get; set; }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
